e03f5f944bfd34392a4326621a0654a805d7d261
qqq <aswd25@yandex.ru>
2020-02-19 20:03:50 +0200

    ВТОРАЯ ПРАВКА.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import os
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
 
-application = get_wsgi_application()
+    application = get_wsgi_application()
